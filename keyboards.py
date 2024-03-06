250027a3b039ab526d665bf41d4881ca9019c851
artyom <pragmatist1618@gmail.com>
2024-03-06 16:58:48 +0300

    update program

---
+++ keyboards.py
@@ -0,0 +1,51 @@
+from telebot import types
+
+keyboard_menu = types.InlineKeyboardMarkup()
+keyboard_menu.add(types.InlineKeyboardButton("Рассчитать стоимость доставки", callback_data="calculate"),
+                  types.InlineKeyboardButton("Контакты", callback_data="contacts"))
+
+keyboard_car_type = types.InlineKeyboardMarkup(row_width=1)
+keyboard_car_type.add(types.InlineKeyboardButton("Газель тент/фургон до 1,5т, 9м³", callback_data="car1"),
+                      types.InlineKeyboardButton("Газель повышенный об. до 1,5т, до 15м³", callback_data="car2"),
+                      types.InlineKeyboardButton("Бычок тент/фургон до 3т, 16м³", callback_data="car3"),
+                      types.InlineKeyboardButton("5-тонник тент/фургон, 20-36м³", callback_data="car4"),
+                      types.InlineKeyboardButton("5-тонник бор, 20-36м³", callback_data="car5"),
+                      types.InlineKeyboardButton("10-тонник тент/фургон, 36м³", callback_data="car6"),
+                      types.InlineKeyboardButton("10-тонник борт, 36м³", callback_data="car7"),
+                      types.InlineKeyboardButton("Ман, Мерседес до 10т, 50м³", callback_data="car8"),
+                      types.InlineKeyboardButton("Еврофура тент до 20т, 82-90м³", callback_data="car9"),
+                      types.InlineKeyboardButton("Еврофура борт до 20т, 82-90м³", callback_data="car10"),
+                      types.InlineKeyboardButton("Автопоезд до 20т, 100-110м³", callback_data="car11"),
+                      types.InlineKeyboardButton("Назад", callback_data="back"))
+
+keyboard_ynbe = types.InlineKeyboardMarkup()
+keyboard_ynbe.add(types.InlineKeyboardButton("Да", callback_data="yes"),
+                  types.InlineKeyboardButton("Нет", callback_data="no"),
+                  types.InlineKeyboardButton("Назад", callback_data="back"),
+                  types.InlineKeyboardButton("Главное меню", callback_data="main_menu"))
+
+keyboard_offloading_5 = types.InlineKeyboardMarkup()
+keyboard_offloading_5.add(types.InlineKeyboardButton("1", callback_data="offloading_1"),
+                          types.InlineKeyboardButton("2", callback_data="offloading_2"),
+                          types.InlineKeyboardButton("3", callback_data="offloading_3"),
+                          types.InlineKeyboardButton("4", callback_data="offloading_4"),
+                          types.InlineKeyboardButton("5", callback_data="offloading_5"),
+                          types.InlineKeyboardButton("Назад", callback_data="back"),
+                          types.InlineKeyboardButton("Главное меню", callback_data="main_menu"))
+
+keyboard_offloading_10 = types.InlineKeyboardMarkup()
+keyboard_offloading_10.add(types.InlineKeyboardButton("1", callback_data="offloading_1"),
+                           types.InlineKeyboardButton("2", callback_data="offloading_2"),
+                           types.InlineKeyboardButton("3", callback_data="offloading_3"),
+                           types.InlineKeyboardButton("4", callback_data="offloading_4"),
+                           types.InlineKeyboardButton("5", callback_data="offloading_5"),
+                           types.InlineKeyboardButton("6", callback_data="offloading_6"),
+                           types.InlineKeyboardButton("7", callback_data="offloading_7"),
+                           types.InlineKeyboardButton("8", callback_data="offloading_8"),
+                           types.InlineKeyboardButton("9", callback_data="offloading_9"),
+                           types.InlineKeyboardButton("10", callback_data="offloading_10"),
+                           types.InlineKeyboardButton("Назад", callback_data="back"),
+                           types.InlineKeyboardButton("Главное меню", callback_data="main_menu"))
+
+keyboard_back = types.InlineKeyboardMarkup()
+keyboard_back.add(types.InlineKeyboardButton("Назад", callback_data="back"))
--- main.py
+++ main.py
@@ -1,111 +1,154 @@
 import os
+
 from django.core.wsgi import get_wsgi_application
-from settings import API_TG, managers
+
+from keyboards import *
+from settings import API_TG
 
 # Установка переменной окружения для настройки Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tg_bot_garant_dostavka.settings')
-
 # Загрузка Django приложения
 application = get_wsgi_application()
-
 import telebot
-from orders.models import Car
+
+# from orders.models import Car
+
 
 # Создаем бота
 bot = telebot.TeleBot(API_TG)
 
+# Состояния
+states = {}
+value_list = {}
 
-# Обработчик команды /start
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Приветствие, краткие пояснения")
+    bot.send_message(message.chat.id, "Привет! Я бот для оформления заказов на доставку. \n "
+                                      "Здесь Вы можете узнать приблизительную стоимость доставки.",
+                     reply_markup=keyboard_menu)
 
 
-# Обработчик выбора типа автомобиля
-@bot.message_handler(commands=['order'])
-def handle_type(message):
-    bot.reply_to(message, "Экспедирование груза (Да/Нет)")
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    chat_id = call.message.chat.id
+    message_id = call.message.message_id
+    # посчитать
+    if call.data == "calculate":
+        states[chat_id] = 'car_type'
+        bot.edit_message_text(text="Выберите тип машины:", message_id=message_id, chat_id=chat_id)
+        bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id, reply_markup=keyboard_car_type)
+    # контакты
+    elif call.data == "contacts":
+        states[chat_id] = 'contacts'
+        bot.edit_message_text(text="Контактная информация:\nТелефон: +123456789\nEmail: example@example.com",
+                              message_id=message_id, chat_id=chat_id)
+        bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id,
+                                      reply_markup=keyboard_back)
+    # тип машины
+    elif call.data in ["car1", "car2", "car3", "car4", "car5", "car6", "car7", "car8", "car9", "car10", "car11"]:
+        states[chat_id] = 'forwarding'
+        bot.edit_message_text(text="Экспедирование груза машина:",
+                              message_id=message_id, chat_id=chat_id)
+        bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id,
+                                      reply_markup=keyboard_ynbe)
+    elif call.data in ["yes", 'no']:
+        states[chat_id] = 'offloading'
+        if call.data == 'yes':
+            bot.edit_message_text(text="Укажите количество мест выгрузки:",
+                                  message_id=message_id, chat_id=chat_id)
+            bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id,
+                                          reply_markup=keyboard_offloading_10)
+        else:
+            bot.edit_message_text(text="Укажите количество мест выгрузки:",
+                                  message_id=message_id, chat_id=chat_id)
+            bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id,
+                                          reply_markup=keyboard_offloading_5)
+    elif call.data in ["offloading_1", "offloading_2", "offloading_3", "offloading_4", "offloading_5", "offloading_6",
+                       "offloading_7", "offloading_8", "offloading_9", "offloading_10"]:
+        states[chat_id] = 'time'
+        off_count = str(call.data)[-1] if str(call.data)[-1] != '0' else 10
+        print(off_count)
+        bot.edit_message_text(text="Экспедирование груза машина:",
+                              message_id=message_id, chat_id=chat_id)
+        bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id,
+                                      reply_markup=keyboard_ynbe)
+    elif call.data == "back":
+        if states[chat_id] == "contacts" or states[chat_id] == "car_type":
+            bot.edit_message_text("Привет! Я бот для оформления заказов на доставку. \n "
+                                  "Здесь Вы можете узнать приблизительную стоимость доставки.", chat_id, message_id)
+            bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id,
+                                          reply_markup=keyboard_menu)
+        elif states[chat_id] == "forwarding":
+            bot.edit_message_text("Выберите тип машины:", chat_id, message_id)
+            bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id,
+                                          reply_markup=keyboard_car_type)
+        elif states[chat_id] == "Offloading":
+            bot.edit_message_text("Экспедирование груза машина:", chat_id, message_id)
+            bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id,
+                                          reply_markup=keyboard_ynbe)
+    elif call.data == "main_menu":
+        bot.edit_message_text("Привет! Я бот для оформления заказов на доставку. \n "
+                              "Здесь Вы можете узнать приблизительную стоимость доставки.", chat_id, message_id)
+        bot.edit_message_reply_markup(chat_id=chat_id, message_id=message_id,
+                                      reply_markup=keyboard_menu)
+
+        # # Создание инлайн-клавиатуры
+        # def create_inline_keyboard():
+        #     keyboard = types.InlineKeyboardMarkup(row_width=3)
+        #     for i in range(0, 10, 3):
+        #         buttons = [types.InlineKeyboardButton(str(j), callback_data=str(j)) for j in range(i, i + 3)]
+        #         keyboard.add(*buttons)
+        #     keyboard.add(types.InlineKeyboardButton("Подтвердить", callback_data="confirm"),
+        #                  types.InlineKeyboardButton("Стереть", callback_data="clear"))
+        #     return keyboard
+        #
+        # # Обработчик команды /start
+        # @bot.message_handler(commands=['start'])
+        # def handle_start(message):
+        #     bot.send_message(message.chat.id, "Введите число от 0 до 9999:", reply_markup=create_inline_keyboard())
+        #
+        # # Обработчик нажатий на кнопки
+        # @bot.callback_query_handler(func=lambda call: True)
+        # def handle_callback_query(call):
+        #     if call.data.isdigit():
+        #         bot.answer_callback_query(call.id, f"Вы выбрали число {call.data}")
+        #     elif call.data == "confirm":
+        #         bot.answer_callback_query(call.id, "Подтверждено")
+        #     elif call.data == "clear":
+        #         bot.answer_callback_query(call.id, "Стерто")
 
 
-# Обработчик ответа на вопрос об экспедировании груза
-@bot.message_handler(func=lambda message: True)
-def handle_expedited_cargo(message):
-    if message.text.lower() == "да":
-        expedited_cargo = True
-    else:
-        expedited_cargo = False
-    bot.reply_to(message, "Выгрузка в ТК с оформлением от 1 до 10 городов (Да/Нет)")
-    bot.register_next_step_handler(message, handle_unloading_in_transport_company, expedited_cargo)
-
-
-# Обработчик ответа на вопрос о выгрузке в ТК
-def handle_unloading_in_transport_company(message, expedited_cargo):
-    if message.text.lower() == "да":
-        unloading_in_transport_company = True
-    else:
-        unloading_in_transport_company = False
-    bot.reply_to(message, "Кол-во мест выгрузки автомобиля (1,2,3,4,5)")
-    bot.register_next_step_handler(message, handle_number_of_unloading_places, expedited_cargo,
-                                   unloading_in_transport_company)
-
-
-# Обработчик ответа на вопрос о количестве мест выгрузки
-def handle_number_of_unloading_places(message, expedited_cargo, unloading_in_transport_company):
-    number_of_unloading_places = int(message.text)
-    bot.reply_to(message, "Количество часов работы автомобиля или ориентировочное время работы")
-    bot.register_next_step_handler(message, handle_hours_of_work, expedited_cargo, unloading_in_transport_company,
-                                   number_of_unloading_places)
-
-
-# Обработчик ответа на вопрос о количестве часов работы
-def handle_hours_of_work(message, expedited_cargo, unloading_in_transport_company, number_of_unloading_places):
-    hours_of_work = message.text
-    bot.reply_to(message, "Количество пробега за МКАД (км)")
-    bot.register_next_step_handler(message, handle_mileage_outside_mkad, expedited_cargo,
-                                   unloading_in_transport_company, number_of_unloading_places, hours_of_work)
-
-
-# Обработчик ответа на вопрос о пробеге за МКАД
-def handle_mileage_outside_mkad(message, expedited_cargo, unloading_in_transport_company, number_of_unloading_places,
-                                hours_of_work):
-    mileage_outside_mkad = int(message.text)
-    bot.reply_to(message, "Введите ваши контактные данные")
-    bot.register_next_step_handler(message, handle_contact_info, expedited_cargo, unloading_in_transport_company,
-                                   number_of_unloading_places, hours_of_work, mileage_outside_mkad)
-
-
-# Обработчик ввода контактных данных
-def handle_contact_info(message, expedited_cargo, unloading_in_transport_company, number_of_unloading_places,
-                        hours_of_work, mileage_outside_mkad):
-    contact_info = message.text
-    # Создаем объект Car и сохраняем его в базе данных
-    car = Car.objects.create(
-        type=message.text,
-        expedited_cargo=expedited_cargo,
-        unloading_in_transport_company=unloading_in_transport_company,
-        number_of_unloading_places=number_of_unloading_places,
-        hours_of_work=hours_of_work,
-        mileage_outside_mkad=mileage_outside_mkad,
-        contact_info=contact_info
-    )
-
-    # Отправляем уведомления менеджерам
-    for manager in managers:
-        bot.send_message(manager, f"Новый заказ:\n{car}")
-
-    body = '{message}\n' \
-           '--\n' \
-           '{first}, {last}\n' \
-           '{username}, {id}'.format(message=message.text, first=message.from_user.first_name,
-                                     last=message.from_user.last_name, username=message.from_user.username,
-                                     id=message.chat.id)
-    # Открываем файл для записи (если файл не существует, он будет создан)
-    with open('orders.log', 'a') as file:
-        # Записываем содержимое переменной body в файл
-        file.write(body + '\n')
-
-    bot.reply_to(message, "Спасибо! Ваш заказ принят.")
-
-
-# Запускаем бота
+# # Обработчик ввода контактных данных
+# def handle_contact_info(message, expedited_cargo, unloading_in_transport_company, number_of_unloading_places,
+#                         hours_of_work, mileage_outside_mkad):
+#     contact_info = message.text
+#     # Создаем объект Car и сохраняем его в базе данных
+#     car = Car.objects.create(
+#         type=message.text,
+#         expedited_cargo=expedited_cargo,
+#         unloading_in_transport_company=unloading_in_transport_company,
+#         number_of_unloading_places=number_of_unloading_places,
+#         hours_of_work=hours_of_work,
+#         mileage_outside_mkad=mileage_outside_mkad,
+#         contact_info=contact_info
+#     )
+#
+#     # Отправляем уведомления менеджерам
+#     for manager in managers:
+#         bot.send_message(manager, f"Новый заказ:\n{car}")
+#
+#     body = '{message}\n' \
+#            '--\n' \
+#            '{first}, {last}\n' \
+#            '{username}, {id}'.format(message=message.text, first=message.from_user.first_name,
+#                                      last=message.from_user.last_name, username=message.from_user.username,
+#                                      id=message.chat.id)
+#     # Открываем файл для записи (если файл не существует, он будет создан)
+#     with open('orders.log', 'a') as file:
+#         # Записываем содержимое переменной body в файл
+#         file.write(body + '\n')
+#
+#     bot.reply_to(message, "Спасибо! Ваш заказ принят.")
+# # Запускаем бота
 bot.polling()
